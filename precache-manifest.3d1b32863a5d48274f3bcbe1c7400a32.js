d85da9ec068670d92a7429161e91a3f03253c743
JoseLRiano <joselriano@yahoo.com>
2020-04-08 02:18:57 -0400

    Updates

---
+++ precache-manifest.3d1b32863a5d48274f3bcbe1c7400a32.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21fd0f0feccd58fa21b1ef51cc22df12",
+    "url": "/ReactReduxMusicList/index.html"
+  },
+  {
+    "revision": "1e45ec02ec766dc6913b",
+    "url": "/ReactReduxMusicList/static/js/2.a905fc3b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ReactReduxMusicList/static/js/2.a905fc3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f938de6029b43891608d",
+    "url": "/ReactReduxMusicList/static/js/main.69b74485.chunk.js"
+  },
+  {
+    "revision": "0dcbd5dce7d0149bd19b",
+    "url": "/ReactReduxMusicList/static/js/runtime-main.4104fd74.js"
+  }
+]);
